193e6ee350625e3af1b29dce9d330e6c1e5f0c63
sudiptodas153 <sudiptodas4093@gmail.com>
2024-02-18 14:08:54 -0800

    start using js

---
+++ file/style.js
@@ -0,0 +1,155 @@
+
+function setBackgroundColor(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-red-500');
+    element.classList.add('bg-green-500')
+   
+}
+
+// A
+function addGreen(){
+setBackgroundColor('A1');
+}
+function addGreen2(){
+setBackgroundColor('A2');
+}
+function addGreen3(){
+setBackgroundColor('A3');
+}
+function addGreen4(){
+setBackgroundColor('A4');
+}
+
+// B
+function addGreen5(){
+setBackgroundColor('B1');
+}
+function addGreen6(){
+setBackgroundColor('B2');
+}
+function addGreen7(){
+setBackgroundColor('B3');
+}
+function addGreen8(){
+setBackgroundColor('B4');
+}
+
+// C
+function addGreen9(){
+setBackgroundColor('C1');
+}
+function addGreen10(){
+setBackgroundColor('C2');
+}
+function addGreen11(){
+setBackgroundColor('C3');
+}
+function addGreen12(){
+setBackgroundColor('C4');
+}
+
+// /D
+function addGreen13(){
+setBackgroundColor('D1');
+}
+function addGreen14(){
+setBackgroundColor('D2');
+}
+function addGreen15(){
+setBackgroundColor('D3');
+}
+function addGreen16(){
+setBackgroundColor('D4');
+}
+
+// E
+function addGreen17(){
+setBackgroundColor('E1');
+}
+function addGreen18(){
+setBackgroundColor('E2');
+}
+function addGreen19(){
+setBackgroundColor('E3');
+}
+function addGreen20(){
+setBackgroundColor('E4');
+}
+
+// F
+function addGreen21(){
+setBackgroundColor('F1');
+}
+function addGreen22(){
+setBackgroundColor('F2');
+}
+function addGreen23(){
+setBackgroundColor('F3');
+}
+function addGreen24(){
+setBackgroundColor('F4');
+}
+
+// G
+function addGreen25(){
+setBackgroundColor('G1');
+}
+function addGreen26(){
+setBackgroundColor('G2');
+}
+function addGreen27(){
+setBackgroundColor('G3');
+}
+function addGreen28(){
+setBackgroundColor('G4');
+}
+
+// /H
+function addGreen29(){
+setBackgroundColor('H1');
+}
+function addGreen30(){
+setBackgroundColor('H2');
+}
+function addGreen31(){
+setBackgroundColor('H3');
+}
+function addGreen32(){
+setBackgroundColor('H4');
+}
+
+// I
+function addGreen33(){
+setBackgroundColor('I1');
+}
+function addGreen34(){
+setBackgroundColor('I2');
+}
+function addGreen35(){
+setBackgroundColor('I3');
+}
+function addGreen36(){
+setBackgroundColor('I4');
+}
+
+// J
+function addGreen37(){
+setBackgroundColor('J1');
+}
+function addGreen38(){
+setBackgroundColor('J2');
+}
+function addGreen39(){
+setBackgroundColor('J3');
+}
+function addGreen40(){
+setBackgroundColor('J4');
+}
+
+function Result(){
+    const mainForm = document.getElementById('main');
+    mainForm.classList.add('hidden');
+    const successed = document.getElementById('success');
+    successed.classList.remove('hidden');
+}
+
